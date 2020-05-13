c0813de570a29c49ba8ec2ec87eaa9095a22b97e
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-05-12 22:59:43 -0300

    Exercícios de Js

--- JavaScript/exercicio9.js
+++ JavaScript/exercicio9.js
@@ -1,3 +1,8 @@
+//Escreva um programa que defina três números em variáveis no seu começo e 
+//retorne true se uma das três for ímpar. Caso contrário, ele retorna false.
+
+//Bonus: use somente um if.
+
 let num1 = 1;
 let num2 = 5;
 let num3 = 10;
