6046a5e3b2f8ed986e936c7d91bd2217fcb3aaf1
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-05-14 15:32:22 -0300

    Exercícios de JS 4_2

--- JavaScript/exercises/4_2/exercicio2.js
+++ JavaScript/exercises/4_2/exercicio2.js
@@ -2,5 +2,8 @@
 //valores contidos no array e imprimir o resultado;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let total = numbers.reduce((total, currentElement) => total + currentElement)
-console.log(total);
+let total = 0;
+for (let i = 0; i < numbers.length; i++){
+    total += numbers[i];
+}console.log(total);
+
---
+++ JavaScript/exercises/4_2/exercicio3.js
@@ -0,0 +1,8 @@
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+
+for (i = 0; i < numbers.length; i++){
+    total += numbers[i];
+}console.log(total/numbers.length);
