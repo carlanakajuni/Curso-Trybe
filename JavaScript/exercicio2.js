d94c82eb8cfa683333b0694b9d6a30caa5aed02d
Carla Nakajuni <63618167+carlanakajuni@users.noreply.github.com>
2020-05-15 11:06:51 -0300

    Delete exercicio2.js

--- JavaScript/exercicio2.js
+++
@@ -1,9 +0,0 @@
-//Faça um programa que retorne o maior de dois números. 
-//Defina no começo do programa duas variáveis com os valores que serão comparados.
-
-let maior = 75;
-let menor = 35;
-
-if(maior > menor){
-    console.log(maior);
-}
